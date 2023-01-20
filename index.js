3d7f3b040573e304bc58328196887999f98ed2b0
jbmiv <91992250+jbmiv@users.noreply.github.com>
2023-01-20 10:20:54 -0800

    button

--- index.js
+++ index.js
@@ -115,3 +115,51 @@ console.log(square(2));
 
 
 //BARRING ALL THE ABOVE CODE I AM GOING TO USE THE NEXT METHODS TO PUSH AN ARRAY OF IMAGES ONTO MY WEBSITE
+
+
+
+
+//to push images:
+/*
+// Create HTMLImageElement
+let myImage = new Image(width, height);
+
+// Assign it an src attribute
+myImage.src = 'https://site.com/example-1.jpeg';
+
+
+NEXT---------
+
+
+// Declare an array object for our array of images
+let arrayOfImages = [];
+
+// Create image object and assign width and height
+let myImage = new Image('1024','768');
+// Assign src attribute to image object
+myImage.src = 'https://site.com/example-1.jpeg';
+
+// Push image object to arrayOfImages
+arrayOfImages.push(myImage);
+
+// Output arrayOfImages to the console
+console.log(arrayOfImages);
+
+
+//Every time you use the Image() constructor, 
+//you create a new instance of an HTMLImageElement. 
+//You can these reference and manipulate that HTMLImageElement as you wish.
+
+
+NEXT------------
+
+
+// Append an <img> element to the <body> tag
+document.body.appendChild(arrayOfImages[0]);
+
+// Insert an <img> element as the inner HTML of div#image-01
+document.querySelector('div#image-01').innerHTML(arrayOfImages[0]);
+
+
+
+*/
